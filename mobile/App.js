b280218638ba21a444aadb2d42a6bed1c4c51093
MarcondesOliveira <marcondescurso@outlook.com>
2020-02-02 09:30:24 -0300

    before formatting

--- mobile/App.js
+++ mobile/App.js
@@ -1,29 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Gato Troll</Text>
-      <Text style={styles.subtitle}>==== Gato Troll!!! ====</Text>
-      <Text style={styles.subtitle}>==== Gato Zezinho !!! ====</Text>
-    </View>
+    <Routes />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#279d55',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 36
-  },
-  subtitle: {
-    color: '#fff',
-    fontWeight: 'bold'
-  }
-});
